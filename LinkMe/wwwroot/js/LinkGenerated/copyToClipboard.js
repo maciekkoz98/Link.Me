b09667ac287313033d9902f53df4ce1da7cfcee0
Maciej Kozlowski <maciekkoz98@gmail.com>
2020-06-03 00:36:21 +0200

    long original link handle, draft of copy to clipboard feature

---
+++ LinkMe/wwwroot/js/LinkGenerated/copyToClipboard.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    var copyButton = document.getElementById("copyButton");
+    var shortLinkInput = document.getElementById("shortLinkInput");
+    copyButton.addEventListener("click", copyToClipboard, true);
+    shortLinkInput.addEventListener("click", copyToClipboard, true);
+}
+
+function copyToClipboard() {
+    console.log("klik");
+    navigator.clipboard.writeText(shortLinkInput.value).then(() => { console.log("udane"); }, (err) => { console.log(err); });
+}
